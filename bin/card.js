bf6bcee2965e35fe3a9b23990e19127a8a6ed4b2
rajikaimal <inbox.rajika@gmail.com>
2018-12-21 10:29:58 +0530

    Change handles

--- bin/card.js
+++ bin/card.js
@@ -14,14 +14,14 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Rajika Imal /'),
+  handle: chalk.cyan('rajikaimal'),
+  work: chalk.white('Software Engineer at 99X Technology'),
+  twitter: chalk.cyan('https://twitter.com/rajikaimal'),
+  github: chalk.cyan('https://github.com/rajikaimal'),
+  linkedin: chalk.cyan('https://linkedin.com/in/rajikaimal'),
+  web: chalk.cyan('https://rajikaimal.github.io'),
+  npx: chalk.white('npx misui'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
